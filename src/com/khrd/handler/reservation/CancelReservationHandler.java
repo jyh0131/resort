a270a22554e6862cfb9e2f6a3958152f50225339
dsgood0 <dsgood0@naver.com>
2019-10-22 16:16:15 +0900

    같음

---
+++ src/com/khrd/handler/reservation/CancelReservationHandler.java
@@ -0,0 +1,38 @@
+package com.khrd.handler.reservation;
+
+import java.sql.Connection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.khrd.controller.CommandHandler;
+import com.khrd.dao.ReservationDAO;
+import com.khrd.jdbc.ConnectionProvider;
+import com.khrd.jdbc.JDBCUtil;
+
+public class CancelReservationHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String sNo = request.getParameter("no");
+		int rsvNo = Integer.parseInt(sNo);
+		
+		Connection conn = null;
+		
+		try {
+			conn = ConnectionProvider.getConnection();
+			ReservationDAO dao = ReservationDAO.getnInstance();
+			dao.cancelReserve(conn, rsvNo);
+			response.sendRedirect(request.getContextPath()+"/reservation/list.do");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(conn);
+		}		
+		
+		return null;
+	}
+
+}
